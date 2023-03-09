704ec87edbcdce1b700122a34a5ed73190d7a37a
paproka <paproka@uma.es>
2023-03-09 16:31:15 +0100

    Project structure

---
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -0,0 +1,51 @@
+package org.mps.deque;
+
+public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
+
+    private DequeNode<T> first;
+    private DequeNode<T> last;
+    private int size;
+
+    public DoublyLinkedListDeque() {
+        
+    }
+
+    @Override
+    public void prepend(T value) {
+        
+    }
+
+    @Override
+    public void append(T value) {
+        
+    }
+
+    @Override
+    public void deleteFirst() {
+       
+
+    }
+
+    @Override
+    public void deleteLast() {
+      
+    }
+
+    @Override
+    public T first() {
+
+        return null;
+    }
+
+    @Override
+    public T last() {
+
+        return null;
+    }
+
+    @Override
+    public int size() {
+
+        return 0;
+    }
+}
